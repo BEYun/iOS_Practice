dbf1faf1180537c6005233f6569fc2aa540dac61
BEYun <yun@yunbyeong-eun-ui-MacBookPro.local>
2022-05-24 17:59:20 +0900

    2022/05/24 UIStackView를 이용한 CallApp 기초UI

---
+++ CallApp/CallApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CallApp
+//
+//  Created by 윤병은 on 2022/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
