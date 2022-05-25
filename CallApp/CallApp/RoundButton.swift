e264b24e6bf645afc488db55e46d74aeccf20789
BEYun <yun@yunbyeong-eun-ui-MacBookPro.local>
2022-05-25 17:37:54 +0900

    2022/05/25 CallApp UI 추가

---
+++ CallApp/CallApp/RoundButton.swift
@@ -0,0 +1,20 @@
+//
+//  RoundButton.swift
+//  CallApp
+//
+//  Created by 윤병은 on 2022/05/25.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundButton: UIButton {
+    @IBInspectable var isRound: Bool = false {
+        didSet {
+            if isRound {
+                self.layer.cornerRadius = self.frame.height / 2
+            }
+        }
+    }
+
+}
--- CallApp/CallApp/ViewController.swift
+++ CallApp/CallApp/ViewController.swift
@@ -8,10 +8,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var callButton: RoundButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
