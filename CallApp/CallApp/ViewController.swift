35021459c33a50fb751bf4d10a19dea763751e35
BEYun <yun@yunbyeong-eun-ui-MacBookPro.local>
2022-05-26 17:55:14 +0900

    2022/05/26 CallApp 번호 액션 추가

--- CallApp/CallApp/ViewController.swift
+++ CallApp/CallApp/ViewController.swift
@@ -8,12 +8,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var callButton: RoundButton!
+    @IBOutlet weak var button_1: UIButton!
+    @IBOutlet weak var button_2: UIButton!
+    @IBOutlet weak var button_3: UIButton!
+    @IBOutlet weak var button_4: UIButton!
+    @IBOutlet weak var button_5: UIButton!
+    @IBOutlet weak var button_6: UIButton!
+    @IBOutlet weak var button_7: UIButton!
+    @IBOutlet weak var button_8: UIButton!
+    @IBOutlet weak var button_9: UIButton!
+    @IBOutlet weak var button_ast: UIButton!
+    @IBOutlet weak var button_0: UIButton!
+    @IBOutlet weak var button_s: UIButton!
+    @IBOutlet weak var button_call: UIButton!
+    @IBOutlet weak var button_del: UIButton!
+    
+    @IBOutlet weak var phoneNumberLabel: UILabel!
+    @IBOutlet weak var addPhoneNumber: UIButton!
+    
+    private var phoneNumber: [String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    func pressNumber(number: UIButton) {
+        if let num = number.titleLabel?.text {
+            phoneNumberLabel.text? += num
+        } else {
+            phoneNumberLabel.text? += "*"
+        }
+    }
+    
+    @IBAction func tapNumberButton(_ sender: UIButton) {
+        if phoneNumberLabel.isHidden == true && addPhoneNumber.isHidden == true {
+            phoneNumberLabel.isHidden = false
+            addPhoneNumber.isHidden = false
+        }
+        pressNumber(number: sender)
+    }
+    
+    @IBAction func tapDelNumberButton(_ sender: UIButton) {
+    }
+    
 }
 
